f1711e76c1f291d81f7a8ee896a143d534597e6c
Brittani M Brundage unknown student <bmb23@afsconnect1.njit.edu>
2016-10-11 15:54:47 -0400

    set up table

--- table.php
+++ table.php
@@ -1,3 +1,22 @@
-<?
+<?php
+$people = array(
+    array(),
+    array(),
+    array(),
+    array(),
+    array()
+);
 
+echo '<table border="4">';
+echo '<tr><th>First Name</th><th>Last Name</th><th>Major</th></tr>';
+foreach( $people as $person )
+{
+    echo '<tr>';
+    foreach( $person as $key )
+    {
+        echo '<td>'.$key.'</td>';
+    }
+    echo '</tr>';
+}
+echo '</table>';
 ?>
