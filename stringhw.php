948f8dd2b4c3b5afef5b1e62f17edd025d704c95
Brittani M Brundage unknown student <bmb23@afsconnect1.njit.edu>
2016-10-18 12:36:05 -0400

    counting chars

--- stringhw.php
+++ stringhw.php
@@ -21,11 +21,20 @@ echo (" <br />");
 //Chunk Split
 
 $str3 = "Hello there, name is Brittani";
-echo chunk_split($str3, 3)."\n";
+echo chunk_split($str3, 3);
 
 echo (" <br />");
 echo (" <br />");
 
+//Count Chars
 
+$counting = "Brittani Marie Brundage";
+
+foreach (count_chars($counting, 1) as $i => $val) {
+   echo "There were $val instance(s) of \"" , chr($i) , "\" in the string.\n";
+}
+
+echo (" <br />");
+echo (" <br />");
 
 ?>
