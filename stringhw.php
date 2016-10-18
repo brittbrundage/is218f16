6cc3f85481c92c6f1253ae0b7f2bf8b3df98a173
Brittani M Brundage unknown student <bmb23@afsconnect1.njit.edu>
2016-10-18 12:32:47 -0400

    chunk split

--- stringhw.php
+++ stringhw.php
@@ -12,6 +12,20 @@ echo (" <br />");
 
 //CHR
 
-echo chr(834)
+echo chr(834);
+
+
+echo (" <br />");
+echo (" <br />");
+
+//Chunk Split
+
+$str3 = "Hello there, name is Brittani";
+echo chunk_split($str3, 3)."\n";
+
+echo (" <br />");
+echo (" <br />");
+
+
 
 ?>
