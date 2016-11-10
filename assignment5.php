fa54632bca0b1b9968b71a0ddca4b9cf12bb7c22
Brittani M Brundage unknown student <bmb23@afsconnect1.njit.edu>
2016-11-10 17:42:40 -0500

    switch statements

--- assignment5.php
+++ assignment5.php
@@ -1,5 +1,8 @@
 <?php
 
+// if statements
+
+echo '<b>If Statements</b><br><br>';
 
 $dog = NULL;
 if(is_null($dog)){
@@ -33,18 +36,66 @@ else{
 }
 echo $truck;
 echo '<br><br>';
-?>
 
 
+// switch statements
 
+echo '<b>Switch Statements</b><br><br>';
 
+$day = 'Monday';
+switch ($day) {
+	
+	case isset($day):
+	echo 'Have a great week!<br><br>';
+	break;
+		
+	default:
+	echo 'Its going to be a good day<br><br>';
+	break;
+	
+}
 
+$hours = 6;
+switch ($hours){
+	case empty($hours):
+	echo 'Sorry no hours found<br><br>';
+	break;
+	
+	default:
+	echo '6 hours<br><br>';
+	
+}
 
-
-
-
-
-
-
-
+$mins = "";
+switch($mins) {
+	case is_null($mins):
+	echo 'Minutes is null<br><br>';
+	break;
+	
+	default:
+	echo 'No Minutes<br><br>';
+	break;
+	
+}
+	
+	
+$weekend = 'Saturday';
+switch($weekend) {
+	case isset($weekend):
+	echo 'It is Saturday!<br><br>';
+	break;
+	
+	case empty($weekend):
+	echo 'Its not the weekend yet.<br><br>';
+	break;	
+	
+	case is_null($weekend):
+	echo 'NULL';
+	break;
+	
+	default:
+	echo 'Weekend just ended!<br><br>';
+	break;
+	
+}
 ?>
