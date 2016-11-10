a9b6e8b9d6794c0b1dcd263ade218fde6b04c039
Brittani M Brundage unknown student <bmb23@afsconnect1.njit.edu>
2016-11-10 12:02:14 -0500

    rest of if statements

--- assignment5.php
+++ assignment5.php
@@ -1,29 +1,41 @@
 <?php
 
-$fish='goldfish';
+
+$dog = NULL;
+if(is_null($dog)){
+  echo 'Dog is Null<br><br>';
+}
+
+$fish = 'goldfish';
 if(isset($fish)){
-  echo 'Goldfish! <br>';
+  echo 'Goldfish! <br><br>';
 }
 else{
-  echo 'No Fish!<br>';
+  echo 'No Fish!<br><br>';
 }
 
 
-$fish='goldfish';
-if(isset($fish)){
-  echo 'Goldfish! <br>';
+$car = '';
+if(empty($car)){
+  echo 'Empty Car Lot. <br><br>';
 }
 else{
-  echo 'No Fish!<br>';
+  echo 'No Cars in Records<br><br>';
 }
 
 
-
-
-
-
-
+$truck = 'jeep';
+if(empty($truck)){
+  echo 'Empty Truck Lot.<br><br>';
+}
+else{
+  $truck='Expedition!<br><br>';
+}
+echo $truck;
+echo '<br><br>';
 ?>
+
+
 
 
 
