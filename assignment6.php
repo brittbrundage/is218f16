7d8ea26689243d025fa63122303c444c4c5135d6
Brittani M Brundage unknown student <bmb23@afsconnect1.njit.edu>
2016-11-15 15:49:41 -0500

    memory useage

--- assignment6.php
+++ assignment6.php
@@ -21,6 +21,21 @@ $files = array_map('realpath',$files);
 print_r($files);
 echo "Glob Function<br><br>";
 
+//Memory Usage Information 
+
+echo "Initial: ".memory_get_usage()." bytes <br>";
+for ($i = 0; $i < 100000; $i++) {
+    $array []= md5($i);
+}
+ 
+for ($i = 0; $i < 100000; $i++) {
+    unset($array[$i]);
+}
+ 
+echo "Final: ".memory_get_usage()." bytes <br>";
+echo "Peak: ".memory_get_peak_usage()." bytes <br><br>";
+
+
 
 
 ?>
