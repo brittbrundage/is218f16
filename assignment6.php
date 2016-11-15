2f8c10a202f03cb0ac007ebf831bdb87e2a8622b
Brittani M Brundage unknown student <bmb23@afsconnect1.njit.edu>
2016-11-15 16:40:29 -0500

    CPU Usage, Magic Constants

--- assignment6.php
+++ assignment6.php
@@ -35,7 +35,31 @@ for ($i = 0; $i < 100000; $i++) {
 echo "Final: ".memory_get_usage()." bytes <br>";
 echo "Peak: ".memory_get_peak_usage()." bytes <br><br>";
 
+//CPU Usage Information 
 
+sleep(3);
+ 
+$data = getrusage();
+echo "User time: ".
+    ($data['ru_utime.tv_sec'] +
+    $data['ru_utime.tv_usec'] / 1000000);
+echo "<br>";
+echo "System time: ".
+    ($data['ru_stime.tv_sec'] +
+    $data['ru_stime.tv_usec'] / 1000000);
+echo "<br>";
+echo "<br>";
+
+//Magic Constants
+
+//require_once('config/database.php');
+my_debug("Debugging...", __LINE__);
+
+function my_debug($msg, $line) {
+    echo "Line $line: $msg<br><br>";
+}
+
+//Generating Unique ID
 
 
 ?>
