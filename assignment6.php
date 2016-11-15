f83c3dd962de816c0c66183bc38d0eba553997e5
Brittani M Brundage unknown student <bmb23@afsconnect1.njit.edu>
2016-11-15 12:54:09 -0500

    optional arguments

--- assignment6.php
+++ assignment6.php
@@ -1,4 +1,22 @@
-<?
+<?php
+
+//Optional Arguments 
+
+function foo () {
+	
+	$args = func_get_args();
+	
+	foreach ($args as $k => $v) {
+		echo "arg".($k+1).": $v<br>";
+	}
+}
+foo();
+foo('hello');
+foo('hi', 'hey', 'hello');
+
+
+
+
 
 
 ?>
