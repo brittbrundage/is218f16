e68156fb373e8eeedf40ab6a7324dea5f6079a50
Brittani M Brundage unknown student <bmb23@afsconnect1.njit.edu>
2016-11-20 11:21:36 -0500

    globals

--- assignment7.php
+++ assignment7.php
@@ -1,7 +1,18 @@
 <?php
 
-echo "Super Globals";
+echo "Super Globals<br><br>";
 
+function globals() {
+    $test = "local variable";
+
+    echo '$test in global scope: ' . $GLOBALS["test"] . "\n";
+	echo "<br><br>";
+    echo '$test in current scope: ' . $test . "\n";
+	echo "<br><br>";
+}
+
+$test = "Example content";
+globals();
 
 
 
