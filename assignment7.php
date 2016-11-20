c17f94361540257b2e8db12a8d91eb76de1b994f
Brittani M Brundage unknown student <bmb23@afsconnect1.njit.edu>
2016-11-20 12:41:09 -0500

    server and get

--- assignment7.php
+++ assignment7.php
@@ -2,6 +2,8 @@
 
 echo "Super Globals<br><br>";
 
+//$GLOBALS
+
 function globals() {
     $test = "local variable";
 
@@ -13,6 +15,21 @@ function globals() {
 
 $test = "Example content";
 globals();
+
+//$_SERVER
+
+echo $_SERVER['SERVER_NAME'];
+
+//$_GET
+
+print_r($_GET);
+if($_GET["a"] === "") echo "a is an empty string\n";
+if($_GET["a"] === false) echo "a is false\n";
+if($_GET["a"] === null) echo "a is null\n";
+if(isset($_GET["a"])) echo "a is set\n";
+if(!empty($_GET["a"])) echo "a is not empty";
+
+echo "<br><br>"
 
 
 
